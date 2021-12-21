643d5411fd1b39d79dd8feee3c109ee020effeb4
njit_77 <512424964@qq.com>
2021-12-21 09:43:17 +0800

    修改测试

--- js/post.js
+++ js/post.js
@@ -1,18 +1,18 @@
 $(function() {
     $('pre').addClass('prettyprint linenums'); //添加Google code Hight需要的class
 
-    window.disqus_shortname = 'beiyuu'; // required: replace example with your forum shortname
+    window.disqus_shortname = 'njit-77'; // required: replace example with your forum shortname
     $('#disqus_container .comment').on('click',function(){
         $(this).html('加载中...');
         var that = this;
-        $.getScript('http://' + disqus_shortname + '.disqus.com/embed.js',function(){$(that).remove()});
+        $.getScript('https://' + disqus_shortname + '.github.io/embed.js',function(){$(that).remove()});
     });
 
     $('.entry a').each(function(index,element){
         var href = $(this).attr('href');
         if(href){
             if(href.indexOf('#') == 0){
-            }else if ( href.indexOf('/') == 0 || href.toLowerCase().indexOf('beiyuu.com')>-1 ){
+            }else if ( href.indexOf('/') == 0 || href.toLowerCase().indexOf('njit-77.github.io/')>-1 ){
             }else if ($(element).has('img').length){
             }else{
                 $(this).attr('target','_blank');
